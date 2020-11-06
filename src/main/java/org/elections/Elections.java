17bf576bdda663b552d1f8f26b9b5fbb2a795eb5
Remy Sanlaville <remy.sanlaville@gmail.com>
2020-11-06 17:42:27 +0100

    Implement a simple election

--- src/main/java/org/elections/Elections.java
+++ src/main/java/org/elections/Elections.java
@@ -1,7 +1,31 @@
 package org.elections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Elections {
-    public static boolean SomethingTrue() {
-        return true;
+    List<String> candidates = new ArrayList<>();
+    ArrayList<Integer> votes = new ArrayList<>();
+
+    public void addCandidate(String candidate) {
+        candidates.add(candidate);
+        votes.add(0);
+    }
+
+    public void voteFor(String elector, String candidate, String electorDistrict) {
+        if (candidates.contains(candidate)) {
+            int index = candidates.indexOf(candidate);
+            votes.set(index, votes.get(index) + 1);
+        }
+    }
+
+    public Map<String, String> results() {
+        Map<String, String> results = new HashMap<>();
+        for (int i = 0; i < votes.size(); i++) {
+            results.put(candidates.get(i), votes.get(i).toString());
+        }
+        return results;
     }
 }
--- src/test/java/org/elections/ElectionsTest.java
+++ src/test/java/org/elections/ElectionsTest.java
@@ -3,10 +3,23 @@ package org.elections;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 class ElectionsTest {
 
     @Test
-    void dummyTest() {
-        Assertions.assertThat(Elections.SomethingTrue()).isTrue();
+    void first(){
+        Elections elections = new Elections();
+        elections.addCandidate("Michel");
+        elections.addCandidate("Jerry");
+        elections.addCandidate("Johnny");
+
+        elections.voteFor("Bob", "Jerry", "Bob's District");
+        elections.voteFor("Anna", "Johnny", "Anna's District");
+
+        Map<String, String> results = elections.results();
+
+        Map<String, String> expectedResults = Map.of("Jerry" , "1", "Johnny", "1", "Michel", "0");
+        Assertions.assertThat(results).isEqualTo(expectedResults);
     }
 }
