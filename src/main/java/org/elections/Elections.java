b48296c72aed4246ad3125662b807db26f18e33f
Remy Sanlaville <remy.sanlaville@gmail.com>
2020-11-09 21:11:15 +0100

    Implement election by districts

--- src/main/java/org/elections/Elections.java
+++ src/main/java/org/elections/Elections.java
@@ -1,5 +1,6 @@
 package org.elections;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,46 +9,129 @@ import java.util.Map;
 public class Elections {
     List<String> candidates = new ArrayList<>();
     List<String> officialCandidates = new ArrayList<>();
-    ArrayList<Integer> votes = new ArrayList<>();
+    ArrayList<Integer> votesWithoutDistricts = new ArrayList<>();
+    Map<String, ArrayList<Integer>> votesWithDistricts;
+    private boolean withDisctrict;
+
+    public Elections(boolean withDistrict) {
+        this.withDisctrict = withDistrict;
+
+        votesWithDistricts = new HashMap<>();
+        votesWithDistricts.put("District 1", new ArrayList<>());
+        votesWithDistricts.put("District 2", new ArrayList<>());
+        votesWithDistricts.put("District 3", new ArrayList<>());
+    }
 
     public void addCandidate(String candidate) {
         officialCandidates.add(candidate);
         candidates.add(candidate);
-        votes.add(0);
+        votesWithoutDistricts.add(0);
+        votesWithDistricts.get("District 1").add(0);
+        votesWithDistricts.get("District 2").add(0);
+        votesWithDistricts.get("District 3").add(0);
     }
 
     public void voteFor(String elector, String candidate, String electorDistrict) {
-        if (candidates.contains(candidate)) {
-            int index = candidates.indexOf(candidate);
-            votes.set(index, votes.get(index) + 1);
+        if (!withDisctrict) {
+            if (candidates.contains(candidate)) {
+                int index = candidates.indexOf(candidate);
+                votesWithoutDistricts.set(index, votesWithoutDistricts.get(index) + 1);
+            } else {
+                candidates.add(candidate);
+                votesWithoutDistricts.add(1);
+            }
         } else {
-            candidates.add(candidate);
-            votes.add(1);
+            if (votesWithDistricts.containsKey(electorDistrict)) {
+                ArrayList<Integer> districtVotes = votesWithDistricts.get(electorDistrict);
+                if (candidates.contains(candidate)) {
+                    int index = candidates.indexOf(candidate);
+                    districtVotes.set(index, districtVotes.get(index) + 1);
+                } else {
+                    candidates.add(candidate);
+                    votesWithDistricts.forEach((district, votes) ->{
+                        votes.add(0);
+                    });
+                    districtVotes.set(candidates.size()-1, districtVotes.get(candidates.size()-1) + 1);
+                }
+            }
         }
     }
 
     public Map<String, String> results() {
         Map<String, String> results = new HashMap<>();
-        Integer nbVotes = votes.stream().reduce(0, Integer::sum);
+        Integer nbVotes = 0;
         Integer nullVotes = 0;
         Integer blankVotes = 0;
         int nbValidVotes = 0;
-        for (int i = 0; i < officialCandidates.size(); i++) {
-            int index = candidates.indexOf(officialCandidates.get(i));
-            nbValidVotes += votes.get(index);
-        }
 
-        for (int i = 0; i < votes.size(); i++) {
-            Integer candidatResult = (votes.get(i) * 100) / nbValidVotes;
-            String candidate = candidates.get(i);
-            if (officialCandidates.contains(candidate)) {
-                results.put(candidate, candidatResult.toString() + "%");
-            } else {
-                if (candidates.get(i).isEmpty()) {
-                    blankVotes += votes.get(i);
+        if (!withDisctrict) {
+            nbVotes = votesWithoutDistricts.stream().reduce(0, Integer::sum);
+            for (int i = 0; i < officialCandidates.size(); i++) {
+                int index = candidates.indexOf(officialCandidates.get(i));
+                nbValidVotes += votesWithoutDistricts.get(index);
+            }
+
+            for (int i = 0; i < votesWithoutDistricts.size(); i++) {
+                Integer candidatResult = (votesWithoutDistricts.get(i) * 100) / nbValidVotes;
+                String candidate = candidates.get(i);
+                if (officialCandidates.contains(candidate)) {
+                    results.put(candidate, candidatResult.toString() + "%");
                 } else {
-                    nullVotes += votes.get(i);
+                    if (candidates.get(i).isEmpty()) {
+                        blankVotes += votesWithoutDistricts.get(i);
+                    } else {
+                        nullVotes += votesWithoutDistricts.get(i);
+                    }
                 }
+            }
+        } else {
+            for (Map.Entry<String, ArrayList<Integer>> entry : votesWithDistricts.entrySet()) {
+                ArrayList<Integer> districtVotes = entry.getValue();
+                nbVotes += districtVotes.stream().reduce(0, Integer::sum);
+            }
+
+            for (int i = 0; i < officialCandidates.size(); i++) {
+                int index = candidates.indexOf(officialCandidates.get(i));
+                for (Map.Entry<String, ArrayList<Integer>> entry : votesWithDistricts.entrySet()) {
+                    ArrayList<Integer> districtVotes = entry.getValue();
+                    nbValidVotes += districtVotes.get(index);
+                }
+            }
+
+            Map<String, Integer> officialCandidatesResult = new HashMap<>();
+            for (int i = 0; i < officialCandidates.size(); i++) {
+                officialCandidatesResult.put(candidates.get(i), 0);
+            }
+            for (Map.Entry<String, ArrayList<Integer>> entry : votesWithDistricts.entrySet()) {
+                ArrayList<Integer> districtResult = new ArrayList<>();
+                ArrayList<Integer> districtVotes = entry.getValue();
+                for (int i = 0; i < districtVotes.size(); i++) {
+                    Integer candidateResult = 0;
+                    if (nbValidVotes != 0)
+                        candidateResult = (districtVotes.get(i) * 100) / nbValidVotes;
+                    String candidate = candidates.get(i);
+                    if (officialCandidates.contains(candidate)) {
+                        districtResult.add(candidateResult);
+                    } else {
+                        if (candidates.get(i).isEmpty()) {
+                            blankVotes += districtVotes.get(i);
+                        } else {
+                            nullVotes += districtVotes.get(i);
+                        }
+                    }
+                }
+                int districtWinnerIndex = 0;
+                for (int i = 1; i < districtResult.size(); i++) {
+                    if (districtResult.get(districtWinnerIndex) < districtResult.get(i))
+                        districtWinnerIndex = i;
+                }
+                officialCandidatesResult.put(candidates.get(districtWinnerIndex), officialCandidatesResult.get(candidates.get(districtWinnerIndex)) + 1);
+            }
+            for (int i = 0; i < officialCandidatesResult.size(); i++) {
+                Float ratioCandidate = ((float) officialCandidatesResult.get(candidates.get(i))) / officialCandidatesResult.size() * 100;
+                DecimalFormat df = new DecimalFormat();
+                df.setMaximumFractionDigits(2);
+                results.put(candidates.get(i), df.format(ratioCandidate) + "%");
             }
         }
 
--- src/test/java/org/elections/ElectionsTest.java
+++ src/test/java/org/elections/ElectionsTest.java
@@ -8,20 +8,20 @@ import java.util.Map;
 class ElectionsTest {
 
     @Test
-    void first() {
-        Elections elections = new Elections();
+    void electionWithoutDistricts() {
+        Elections elections = new Elections(false);
         elections.addCandidate("Michel");
         elections.addCandidate("Jerry");
         elections.addCandidate("Johnny");
 
-        elections.voteFor("Bob", "Jerry", "Bob's District");
-        elections.voteFor("Jerry", "Jerry", "Jerry's District");
-        elections.voteFor("Anna", "Johnny", "Anna's District");
-        elections.voteFor("Johnny", "Johnny", "Johnny's District");
-        elections.voteFor("Matt", "Donald", "Matt's District");
-        elections.voteFor("Jess", "Joe", "Jess's District");
-        elections.voteFor("Simon", "", "Simon's District");
-        elections.voteFor("Carole", "", "Carole's District");
+        elections.voteFor("Bob", "Jerry", "District 1");
+        elections.voteFor("Jerry", "Jerry", "District 2");
+        elections.voteFor("Anna", "Johnny", "District 1");
+        elections.voteFor("Johnny", "Johnny", "District 3");
+        elections.voteFor("Matt", "Donald", "District 3");
+        elections.voteFor("Jess", "Joe", "District 1");
+        elections.voteFor("Simon", "", "District 2");
+        elections.voteFor("Carole", "", "District 3");
 
         Map<String, String> results = elections.results();
 
@@ -31,6 +31,34 @@ class ElectionsTest {
                 "Michel", "0%",
                 "Blank", "25%",
                 "Null", "25%");
+        Assertions.assertThat(results).isEqualTo(expectedResults);
+    }
+
+    @Test
+    void electionWithDistricts() {
+        Elections elections = new Elections(true);
+        elections.addCandidate("Michel");
+        elections.addCandidate("Jerry");
+        elections.addCandidate("Johnny");
+
+        elections.voteFor("Bob", "Jerry", "District 1");
+        elections.voteFor("Jerry", "Jerry", "District 2");
+        elections.voteFor("Anna", "Johnny", "District 1");
+        elections.voteFor("Johnny", "Johnny", "District 3");
+        elections.voteFor("Matt", "Donald", "District 3");
+        elections.voteFor("Jess", "Joe", "District 1");
+        elections.voteFor("July", "Jerry", "District 1");
+        elections.voteFor("Simon", "", "District 2");
+        elections.voteFor("Carole", "", "District 3");
+
+        Map<String, String> results = elections.results();
+
+        Map<String, String> expectedResults = Map.of(
+                "Jerry", "66,67%",
+                "Johnny", "33,33%",
+                "Michel", "0%",
+                "Blank", "22%",
+                "Null", "22%");
         Assertions.assertThat(results).isEqualTo(expectedResults);
     }
 }
